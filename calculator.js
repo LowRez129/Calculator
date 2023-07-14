f48a06888730422461ef9026c4c0065412068bb8
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-07-14 21:31:50 +1200

    Edit All

--- calculator.js
+++ calculator.js
@@ -1,12 +1,15 @@
 let input = new Array;
 let add_input = new Array;
 let joined_input;
+const screen = document.querySelector(".screen");
 
 function calculator_buttons() {
     const one = document.querySelector(".button-1");
     const two = document.querySelector(".button-2");
     const three = document.querySelector(".button-3");
-    //console.log(one);
+    const button_add = document.querySelector(".button-add");
+    const button_operate = document.querySelector(".button-operate");
+
     one.addEventListener("click", () => {
         Input(one.value);
     });
@@ -18,23 +21,31 @@ function calculator_buttons() {
     three.addEventListener("click", () => {
         Input(three.value);
     });
+
+    button_add.addEventListener("click", add);
+    button_operate.addEventListener("click", operate);
+
+    screen.textContent = "Empty Input";
 }
 
 function Input(value) {
-    const screen = document.querySelector(".screen");
-
     input.push(value);
     joined_input = input.join("");
     screen.textContent = `${joined_input}`;
 }
 
 function add() {
-    const button_add = document.querySelector(".button-+");
-    add_input = joined_input;
+    if (joined_input == null) {
+        return;
+    } 
+    add_input.push(parseInt(joined_input));
+    console.log(add_input);
+    input = [];
+    screen.textContent = `${add_input.join(" + ")}`;
 }
 
-function operate(input, add) {
-    const button_operate = document.querySelector(".button-=");
+function operate() {
+    //screen.textContent = `${add_input}`;
 }
 
 calculator_buttons();
