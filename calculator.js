b55139b61a9e6d475324296105532c34b96b756b
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-07-15 16:42:58 +1200

    Edit All

--- calculator.js
+++ calculator.js
@@ -1,14 +1,26 @@
-let input = new Array;
-let add_input = new Array;
-let joined_input;
-const screen = document.querySelector(".screen");
+let input = ["0"];
+let add_input = [];
+let joined_input = input;
+let evaluation;
+
+const SCREEN = document.querySelector(".screen");
+const SCREEN_OPERATIONS = document.querySelector(".screen-operations");
+
+function SCREEN_RESET(){
+    
+};
 
 function calculator_buttons() {
+    const zero = document.querySelector(".button-0");
     const one = document.querySelector(".button-1");
     const two = document.querySelector(".button-2");
     const three = document.querySelector(".button-3");
     const button_add = document.querySelector(".button-add");
     const button_operate = document.querySelector(".button-operate");
+
+    zero.addEventListener("click", () => {
+        Input(zero.value);
+    })
 
     one.addEventListener("click", () => {
         Input(one.value);
@@ -25,27 +37,52 @@ function calculator_buttons() {
     button_add.addEventListener("click", add);
     button_operate.addEventListener("click", operate);
 
-    screen.textContent = "Empty Input";
+    SCREEN_OPERATIONS.textContent = "- - - - - -";
+    SCREEN.textContent = joined_input;
 }
 
 function Input(value) {
-    input.push(value);
+    switch (input[0]) {
+        case "0":
+            input[0] = value;
+            break;
+        default:
+            input.push(value);
+    }
+   
     joined_input = input.join("");
-    screen.textContent = `${joined_input}`;
+    SCREEN.textContent = `${joined_input}`;
 }
 
 function add() {
-    if (joined_input == null) {
-        return;
-    } 
     add_input.push(parseInt(joined_input));
-    console.log(add_input);
-    input = [];
-    screen.textContent = `${add_input.join(" + ")}`;
+    input = ["0"];
+    joined_input = input;
+    SCREEN_OPERATIONS.textContent = `${add_input.join(" + ")}`;
+    SCREEN.textContent = input;
 }
 
 function operate() {
-    //screen.textContent = `${add_input}`;
+    let all = parseInt(joined_input);
+    
+    add_input.forEach((value) => {
+        all += value;
+        console.log(value);
+    })
+
+    
+    evaluation = all;
+    console.log(input);
+    clear();
+    SCREEN_OPERATIONS.textContent = "- - - - - -";
+    SCREEN.textContent = `= ${all}`;
+
+}
+
+function clear() {
+    add_input = [];
+    input = ["0"];
+    joined_input = input;
 }
 
 calculator_buttons();
