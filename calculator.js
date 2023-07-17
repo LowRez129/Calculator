736b9035d340d64a4bd3a8dfd3c8237b496cb570
LowRez129 <114649755+LowRez129@users.noreply.github.com>
2023-07-17 14:12:59 +1200

    Edit ALL

--- calculator.js
+++ calculator.js
@@ -1,8 +1,8 @@
-let input = [0];
-let add_input = [];
-let minus_input = [];
-
-let evaluation;
+let evaluate = {
+    input: [0],
+    addition: [],
+    subtraction: [],
+}
 
 const SCREEN = document.querySelector(".screen");
 const SCREEN_OPERATIONS = document.querySelector(".screen-operations");
@@ -13,12 +13,12 @@ function calculator_buttons() {
     const two = document.querySelector(".button-2");
     const three = document.querySelector(".button-3");
     const button_add = document.querySelector(".button-add");
-    const button_minus = document.querySelector(".button-minus");
+    const button_subtract = document.querySelector(".button-subtract");
     const button_operate = document.querySelector(".button-operate");
 
     zero.addEventListener("click", () => {
         Input(zero.value);
-    })
+    });
 
     one.addEventListener("click", () => {
         Input(one.value);
@@ -32,72 +32,38 @@ function calculator_buttons() {
         Input(three.value);
     });
 
-    button_add.addEventListener("click", add);
-    button_minus.addEventListener("click", minus);
-    button_operate.addEventListener("click", operate);
+    button_add.addEventListener("click", Add);
+    button_subtract.addEventListener("click", Subtract);
+    button_operate.addEventListener("click", Operate);
 
-    SCREEN_OPERATIONS.textContent = "- - - - - -";
-    SCREEN.textContent = input;
+    SCREEN_OPERATIONS.textContent = " - - - - - - ";
+    SCREEN.textContent = evaluate.input;
 }
 
 function Input(value) {
-    switch (input[0]) {
+    let parseInt_value = parseInt(value);
+    console.log(parseInt_value);
+    switch (evaluate.input[0]) {
         case 0:
-            input[0] = value;
+            evaluate.input[0] = parseInt_value;
             break;
         default:
-            input.push(value);
+            evaluate.input.push(parseInt_value);
     }
-   
-    SCREEN.textContent = `${input.join("")}`;
+
+    SCREEN.textContent = evaluate.input.join("");
 }
 
-function parsedInt_joined_input() {
-    let joined_input = input.join("");
-    return parseInt(joined_input);
-}
-
-function add() {
-    add_input.push(parsedInt_joined_input());
-    clear("input");
-    SCREEN_OPERATIONS.textContent = `${add_input.join(" + ")}`;
-    SCREEN.textContent = input;
-}
-
-function minus() {
-    minus_input.push(parsedInt_joined_input());
-    clear("input");
-    SCREEN_OPERATIONS.textContent = `${minus_input.join(" - ")}`;
-    SCREEN.textContent = input;
-}
-
-function operate() {
-    let all = parsedInt_joined_input();
-    
-    add_input.forEach((value) => {
-        all += value;
-    });
-
-    minus_input.forEach((value) => {
-        all -= value;
-    });
-    
-    clear();
-    SCREEN_OPERATIONS.textContent = "- - - - - -";
-    SCREEN.textContent = `= ${all}`;
+function Add() {
 
 }
 
-function clear(value) {
-    input = [0];
-    switch (value) {
-        case "input":
-            return input;
+function Subtract() {
 
-        default:
-            add_input = input;
-            minus_input = input;
-    }
+}
+
+function Operate() {
+
 }
 
 calculator_buttons();
